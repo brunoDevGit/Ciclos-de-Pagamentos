807ebd03c63ce432126cd8d44e0c1375c55c2553
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-02-03 13:41:30 -0300

    Criando arquivos index e App

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,13 @@
+
+import '../common/template/dependencies' 
+import React from 'react'
+
+export default props => (
+    <div className='wrapper'>
+        <h1>App </h1>
+    </div>
+)
+
+
+
+                /// FAZER COMMIT E PUSH 03-02-21
