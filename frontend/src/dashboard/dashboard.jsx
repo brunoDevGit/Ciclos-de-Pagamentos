da16a4ae95a9a566bf7fc2d250e6a11dfcc8af3f
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-02-14 19:07:10 -0300

    Corrigindo Bugs em Integrando DashBoard com Redux P1

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,5 +1,5 @@
-import React,{Component} from 'react'
-import {connect} from 'react-redux'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -8,18 +8,18 @@ import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render() {
-        const {credit, debt} = this.props.summary
-        return(
+        const { credit, debt } = this.props.summary
+        return (
             <div>
-                <ContentHeader title='Dashboard' small='Versão 1.0'/>
+                <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
                     <Row>
-                   <ValueBox cols=' 4' color='green' icon= 'bank'
-                   value={`R$ ${credit}`} text= 'Total de Creditos'/>
-                   <ValueBox cols=' 4' color='red' icon= 'credit-card'
-                   value={`R$ ${debt}`} text= 'Total de Debitos'/>
-                    <ValueBox cols=' 4' color='blue' icon= 'money'
-                   value={`R$ ${credit - debt}`} text= 'Valor Consolidado'/>
+                        <ValueBox cols=' 4' color='green' icon='bank'
+                            value={`R$ ${credit}`} text='Total de Creditos' />
+                        <ValueBox cols=' 4' color='red' icon='credit-card'
+                            value={`R$ ${debt}`} text='Total de Debitos' />
+                        <ValueBox cols=' 4' color='blue' icon='money'
+                            value={`R$ ${credit - debt}`} text='Valor Consolidado' />
                     </Row>
                 </Content>
             </div>
@@ -27,7 +27,6 @@ class Dashboard extends Component {
     }
 }
 
-const mapStateToProps= state => ({summary: state.dashboard.summary})
+const mapStateToProps = state => ({ summary: state.dashboard.summary })
 
 export default connect(mapStateToProps)(Dashboard)
-export default Dashboard
