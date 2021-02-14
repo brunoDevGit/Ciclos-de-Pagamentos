171b15b123fa05ce6db9b02f434fab0e2a868306
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-02-13 23:15:03 -0300

    Integrando Dashboard com Redux P1

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,26 +1,33 @@
 import React,{Component} from 'react'
+import {connect} from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
+
 class Dashboard extends Component {
     render() {
+        const {credit, debt} = this.props.summary
         return(
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0'/>
                 <Content>
                     <Row>
                    <ValueBox cols=' 4' color='green' icon= 'bank'
-                   value='R$ 10' text= 'Total de Creditos'/>
+                   value={`R$ ${credit}`} text= 'Total de Creditos'/>
                    <ValueBox cols=' 4' color='red' icon= 'credit-card'
-                   value='R$ 10' text= 'Total de Debitos'/>
+                   value={`R$ ${debt}`} text= 'Total de Debitos'/>
                     <ValueBox cols=' 4' color='blue' icon= 'money'
-                   value='R$ 0' text= 'Valor Consolidado'/>
+                   value={`R$ ${credit - debt}`} text= 'Valor Consolidado'/>
                     </Row>
                 </Content>
             </div>
         )
     }
 }
+
+const mapStateToProps= state => ({summary: state.dashboard.summary})
+
+export default connect(mapStateToProps)(Dashboard)
 export default Dashboard
