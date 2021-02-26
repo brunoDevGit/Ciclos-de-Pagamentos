737bccd96f1ce7e0977b75fcf9e29d3ed7e2e60a
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-02-26 15:35:20 -0300

    Componentes Estruturais das Abas

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default props => (
-    <div>
-        <h1>
-            Ciclo de Pagamentos
-        </h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+import Tabs from '../common/Tab/tabs'
+import TabsHeader from '../common/Tab/tabsHeader'
+import TabsContent from '../common/Tab/tabsContent'
+
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader 
+                title='Ciclos de Pagamentos'
+                small='Cadastro'/>
+                <Content>
+                    
+                    <Tabs>
+                        <TabsHeader></TabsHeader>
+                        <TabsContent></TabsContent>
+                    </Tabs>
+                    
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycle
