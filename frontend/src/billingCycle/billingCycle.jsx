e29521b2961f7be6b52e25c226bb3ac747f2fbf7
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-03-01 14:22:29 -0300

    Tab header

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,26 +1,33 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from '../common/Tab/tabs'
 import TabsHeader from '../common/Tab/tabsHeader'
 import TabsContent from '../common/Tab/tabsContent'
-
+import TabHeader from '../common/Tab/tabHeader'
 
 class BillingCycle extends Component {
     render() {
         return (
             <div>
-                <ContentHeader 
-                title='Ciclos de Pagamentos'
-                small='Cadastro'/>
+                <ContentHeader
+                    title='Ciclos de Pagamentos'
+                    small='Cadastro' />
                 <Content>
-                    
+
                     <Tabs>
-                        <TabsHeader></TabsHeader>
+                        <TabsHeader>
+
+       <TabHeader label='Listar' icon='bars' target='tabList' />
+       <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+       <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+       <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+
+                        </TabsHeader>
                         <TabsContent></TabsContent>
                     </Tabs>
-                    
+
                 </Content>
             </div>
         )
