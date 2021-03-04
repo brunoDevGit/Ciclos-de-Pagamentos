1462f4ba0f828f062a93c25cf46ae604ef782038
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-03-04 15:48:22 -0300

    Componente Tab Content

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -6,6 +6,7 @@ import Tabs from '../common/Tab/tabs'
 import TabsHeader from '../common/Tab/tabsHeader'
 import TabsContent from '../common/Tab/tabsContent'
 import TabHeader from '../common/Tab/tabHeader'
+import TabContent from '../common/Tab/tabContent'
 
 class BillingCycle extends Component {
     render() {
@@ -19,13 +20,18 @@ class BillingCycle extends Component {
                     <Tabs>
                         <TabsHeader>
 
-       <TabHeader label='Listar' icon='bars' target='tabList' />
-       <TabHeader label='Incluir' icon='plus' target='tabCreate' />
-       <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
-       <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
 
                         </TabsHeader>
-                        <TabsContent></TabsContent>
+                        <TabsContent>
+                            <TabContent id='tabList'> <h1>Listar</h1> </TabContent>
+                            <TabContent id='tabCreate'> <h1>Incluir</h1> </TabContent>
+                            <TabContent id='tabUpdate'> <h1>Alterar</h1> </TabContent>
+                            <TabContent id='tabDelete'> <h1>Excluir</h1> </TabContent>
+                        </TabsContent>
                     </Tabs>
 
                 </Content>
