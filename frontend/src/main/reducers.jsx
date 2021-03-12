8266ebbd0151025a7581ac0e61281a8d667784e6
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-03-12 17:10:16 -0300

    Criando Action e o Reducer

--- frontend/src/main/reducers.jsx
+++ frontend/src/main/reducers.jsx
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/Tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer
 })
 
 export default rootReducer
