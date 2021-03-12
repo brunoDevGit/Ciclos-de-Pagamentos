a6c04e4acf998fe3c9302e3475bcc05db1759fcb
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-03-12 17:18:56 -0300

    finish

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,4 +1,3 @@
-
 const port = 3003
 
 const bodyParser = require('body-parser')
@@ -7,14 +6,13 @@ const server = express()
 const allowCors = require('./cors')
 const queryParser = require('express-query-int')
 
-
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
 server.use(queryParser())
 
-server.listen(port, function () {
-    console.log(`Backend rodando codigo na porta ${port}.`)
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
 
 module.exports = server
