92d986abfb5b7d54b05ec00a3e4bcaee5d9b9e38
brunoDevGit <brunodoliveirapereira@gmail.com>
2021-03-19 11:43:31 -0300

    Componente Field

---
+++ frontend/src/common/form/labelAndInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Grid from '../layout/grid'
+
+export default props => (
+    <Grid>
+        <div className="form-group">
+            <label htmlFor={props.nme}>{props.label}</label>
+            <input {...props.input} 
+                className='form-control'
+                placeholder={props.placeholder} 
+                readOnly={props.readOnly}
+                type={props.type}
+            />
+        </div>
+    </Grid>
+)
